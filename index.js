0484490c74a3477fa66d1b2fb2dfe134eec20dd8
Anthony Chernikov <achernikov@acvos.com>
2016-07-29 21:11:51 -0700

    Fixing function signature

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 var curry = require("curry");
 
-function find(func) {
-  return function (list) {
-    for (var i in list) {
-      if (func(list[i])) {
-        return list[i];
-      }
+function find(func,list) {
+  for (var i in list) {
+    if (func(list[i])) {
+      return list[i];
     }
+  }
 
-    return undefined;
-  };
+  return undefined;
 }
 
 module.exports = curry(find);
